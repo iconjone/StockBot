1e32f3e3e1f0284e7ef6666292012ca147a78519
Jonathan <jone.bala@gmail.com>
2021-01-14 16:23:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -672,16 +672,17 @@ function startTrading(overRideMode) {
                 (lows.reduce((a, b) => a + b, 0) / lows.length) * 0.25
               );
 
-            bot.sendImage(
-              `Bought at: ${priceBought} | Breakeven Price: ${wantedPrice} | Estimated Sell Price: $${riseStart} | Estimated Jump $${
-                riseStart - priceBought
-              } | Estimated Profit ${
-                sellableVolume * (riseStart - priceBought)
-              } - Volume: ${sellableVolume}${tradingSymbol} - Current Price: ${
-                pricesList[pricesList.length - 1]
-              }`,
-              helper.generateChartPoint(pricesList, wantedPrice)
-            );
+            text = `Bought at: ${priceBought} | Breakeven Price: ${wantedPrice} | Estimated Sell Price: $${riseStart} | Estimated Jump $${
+              riseStart - priceBought
+            } | Estimated Profit ${
+              sellableVolume * (riseStart - priceBought)
+            } - Volume: ${sellableVolume}${tradingSymbol} - Current Price: ${
+              pricesList[pricesList.length - 1]
+            }`;
+
+            image = helper.generateChartPoint(pricesList, wantedPrice);
+
+            bot.sendImage(text, image);
 
             //Start reading the Wewsocket connections
 
